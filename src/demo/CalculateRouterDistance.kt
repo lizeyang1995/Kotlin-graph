d137ff2ef5b425423b2ea5694572cacfd491d5f8
lizeyang1995 <424011527@qq.com>
2020-12-19 01:00:59 +0800

    first commit

---
+++ src/demo/CalculateRouterDistance.kt
@@ -0,0 +1,26 @@
+fun main() {
+
+    calculateRouterDistance("A-D-C")
+
+}
+
+fun calculateRouterDistance(route: String): Unit {
+    val splitedRoute = route.split('-')
+    val stopsNumber = splitedRoute.size
+    val graph = mutableListOf<String>("AB5", "BC4", "CD8", "DC8", "DE6", "AD5", "CE2", "EB3", "AE7")
+    var distance: Int = 0
+    for (index in 0 until stopsNumber - 1) {
+        val beforeLoop = distance
+        graph.forEach {
+            if (it.startsWith(splitedRoute[index].single()) && it[1] == splitedRoute[index + 1].single()) {
+                distance += it[2].toInt() - '0'.toInt()
+            }
+        }
+        if (beforeLoop == distance) {
+            println("no such route")
+            return
+        }
+    }
+    println(distance)
+
+}
